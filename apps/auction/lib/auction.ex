deeda502feb7ac83c3bdbf3aec2348a64dec0284
Cory Westerfield <cory.westerfield@gmail.com>
2020-05-13 08:12:44 -0600

    Add function to check if user exists

--- apps/auction/lib/auction.ex
+++ apps/auction/lib/auction.ex
@@ -1,5 +1,5 @@
 defmodule Auction do
-  alias Auction.{Repo, Item, User}
+  alias Auction.{Repo, Item, User, Password}
 
   def list_items do
     Repo.all(Item)
@@ -36,6 +36,16 @@ defmodule Auction do
   end
 
   def get_user(id), do: Repo.get!(User, id)
+
+  def get_user_by_username_and_password(username, password) do
+    with user when not is_nil(user) <- Repo.get_by(User, %{username: username}),
+         true <- Password.verify_with_hash(password, user.hashed_password) do
+      user
+
+    else
+      _ -> Password.dummy_verify
+    end
+  end
 
   def new_user(), do: User.changeset_with_password(%User{})
 
